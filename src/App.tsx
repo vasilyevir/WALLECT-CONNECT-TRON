e09d8e603db41586736c0316bae5cbc7d60532ce
vasilyevir <vasilyevir@yandex.ru>
2023-06-09 16:31:48 +0300

    FUUUUUUUUUCK YEAAAAAAA!!!!!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css'
-import {WalletConnectChainID, WalletConnectWallet} from "@tronweb3/walletconnect-tron";
+import { WalletConnectAdapter } from "@tronweb3/tronwallet-adapter-walletconnect";
 import {useState} from "react";
 import TronWeb from 'tronweb';
+import { Buffer } from 'buffer';
 
+// @ts-ignore
 export function App() {
   const [addressUser, setAddressUser] = useState('');
-  const wallet = new WalletConnectWallet({
-    network: WalletConnectChainID.Mainnet,
+  const adapter = new WalletConnectAdapter({
+    network: 'Mainnet',
     options: {
       relayUrl: 'wss://relay.walletconnect.com',
       projectId: 'c9024b147776f06d4fa6e75a68c0e220',
@@ -24,22 +26,33 @@ export function App() {
     fullHost: 'https://api.trongrid.io',
     headers: { 'TRON-PRO-API-KEY': 'faafc2f3-1510-4a3b-88a4-ce173fa433d8' },
   });
-  console.log(tronWeb)
+
+  // @ts-ignore
+  window.Buffer = Buffer;
 
   const connect = async () => {
-    const { address } = await wallet.connect();
-    setAddressUser(address)
+    await adapter.connect();
+    setAddressUser(adapter.address || '')
   }
 
   const approve = async () => {
     // const contract = await tronWeb.contract('TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t')
     // console.log(contract)
     // console.log(tronWeb)
+    const walletconnectAdapter = adapter
     try {
-      const unSignedTransaction = await tronWeb.transactionBuilder.sendTrx('TU8tu9ihY4PzGvsz8WYV4BPXKwVQJFSChY', 100, 'TAVTYWfbRzYuCppqgHcshxhaCbVGUWqdc5');
-      console.log(unSignedTransaction)
-      const signedTransaction = await wallet.signTransaction(unSignedTransaction);
-      console.log(signedTransaction)
+      const functionSelector = 'transfer(address,uint256)';
+      const parameter = [{type:'address',value: addressUser},{type:'uint256',value:1}]
+      const tx = await tronWeb.transactionBuilder.triggerSmartContract('TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t', functionSelector, {
+        feeLimit: 1_000_000,
+        callValue: tronWeb.toSun(0.1)
+      }, parameter, 'TAVTYWfbRzYuCppqgHcshxhaCbVGUWqdc5');
+      console.log('tx', tx)
+      //@ts-ignore
+      // const signedTx = await
+      console.log('signedTx', await walletconnectAdapter.signTransaction(tx.transaction))
+      // const result = await tronWeb.trx.sendRawTransaction(signedTx);
+      // console.log('result', result)
     } catch (e) {
       console.log('e:', e)
     }
